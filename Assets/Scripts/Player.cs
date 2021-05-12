80e6ac4ed7a6dacd5a643ffc81a06a3afc7c57df
Ray <79296266+mushroomsouls@users.noreply.github.com>
2021-05-12 16:04:05 +1000

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private bool isGrounded, isJumping;
+    private Rigidbody2D rb;
+    [SerializeField] private float speedMultiplier = 0f;
+    public float jumpVelocity = 10f, jumpTimerCounter, jumpTime;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            isJumping = true;
+            isGrounded = false;
+            jumpTimerCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+        else if (isJumping == true)
+        {
+            if (jumpTimerCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpVelocity;
+                jumpTimerCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speedMultiplier);
+            if (speedMultiplier <= 15f)
+               speedMultiplier += Time.deltaTime * 15f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+             transform.Translate(Vector2.right * Time.deltaTime * speedMultiplier);
+             if (speedMultiplier <= 15f)
+                speedMultiplier += Time.deltaTime * 15f;
+        }
+        else
+        {
+            if (speedMultiplier >= 0f)
+                speedMultiplier -= Time.deltaTime * 25f;
+        } 
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            speedMultiplier = 3f;
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            speedMultiplier = 3f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+}
