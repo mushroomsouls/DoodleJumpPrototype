a565f2a275a5ca490c235a6795f4eed196cb99dc
Rei <79296266+mushroomsouls@users.noreply.github.com>
2021-06-02 08:00:48 +1000

    Add files via upload

---
+++ Assets/prototype/Scripts/PlatformGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformHeight;
+
+    private void Start()
+    {
+        platformHeight = thePlatform.GetComponent<BoxCollider2D>().size.y;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < generationPoint.position.y)
+        {
+            transform.position = new Vector3(Random.Range(-9f, 9f), transform.position.y + platformHeight + distanceBetween, transform.position.x);
+
+            Instantiate(thePlatform, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ Assets/prototype/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private bool isGrounded, isJumping;
+    public float jumpVelocity = 10f;
+
+    float movement = 0f;
+    public float movementSpeed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            isJumping = true;
+            isGrounded = false;
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+        else if (isJumping == true)
+        {
+            isJumping = false;
+        }
+
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+
+}
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+}
